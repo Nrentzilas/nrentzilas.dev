f4a218c56728dd812dabd1a923c728d205332e13
Nrentzilas <nrentzilas@gmail.com>
2024-12-30 00:17:19 +0200

    Added some shit

---
+++ src/components/textReveal.tsx
@@ -0,0 +1,9 @@
+import TextReveal from "@/components/ui/text-reveal";
+
+export function TextRevealDemo() {
+  return (
+    <div className="z-10 flex min-h-64 items-center justify-center rounded-lg border bg-white dark:bg-black">
+      <TextReveal text="Get the fck out of here Mhtso. Before I SPASW YOUR DONTIA" />
+    </div>
+  );
+}
