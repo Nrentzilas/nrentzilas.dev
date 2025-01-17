033e1b0eb0ae7f9e0968d22b2d8a65308570af3f
Nrentzilas <nrentzilas@gmail.com>
2025-01-17 12:00:00 +0200

    17th

--- src/components/small_components/textReveal.tsx
+++ src/components/small_components/textReveal.tsx
@@ -3,7 +3,7 @@ import TextReveal from "@/components/ui/text-reveal";
 export function TextRevealDemo() {
   return (
     <div className="hidden md:flex min-h-64 items-center justify-center rounded-lg bg-white dark:bg-primary-dark">
-      <TextReveal text="Will" />
+      <TextReveal text="Will add" />
     </div>
   );
 }
