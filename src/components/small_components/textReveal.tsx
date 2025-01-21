0f4f1333b5e9d93b2d747ae15a522f8ed5f3c046
Nrentzilas <nrentzilas@gmail.com>
2025-01-21 12:00:00 +0200

    21th

--- src/components/small_components/textReveal.tsx
+++ src/components/small_components/textReveal.tsx
@@ -3,7 +3,7 @@ import TextReveal from "@/components/ui/text-reveal";
 export function TextRevealDemo() {
   return (
     <div className="hidden md:flex min-h-64 items-center justify-center rounded-lg bg-white dark:bg-primary-dark">
-      <TextReveal text="Will add more stuff as time goes by, so" />
+      <TextReveal text="Will add more stuff as time goes by, so don't" />
     </div>
   );
 }
